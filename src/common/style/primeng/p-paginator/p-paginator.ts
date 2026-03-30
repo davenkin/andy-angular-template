0a646efe2ed0a688e7691919a4e2d9097cb09353
Teng Yun <davenkin@126.com>
2026-03-30 20:09:02 +0800

    update

--- src/common/style/primeng/p-paginator/p-paginator.ts
+++ src/common/style/primeng/p-paginator/p-paginator.ts
@@ -1,5 +1,12 @@
 export default {
   navButton: {
     borderRadius: 'var(--4px)',
+    focusRing: {
+      width: 'var(--2px)',
+      style: 'solid',
+      color: 'var(--primary-color)',
+      offset: 'var(--2px)',
+      shadow: 'none',
+    },
   },
 };
